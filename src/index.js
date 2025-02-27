97f26e099ea140751a3cf6555e881cfdcddd773f
heddi <heddiabbaci@gmail.com>
2025-02-27 14:07:21 +0100

    ajout du personnage fille

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {ArcRotateCamera, AssetsManager, Color3, Engine, HemisphericLight, MeshBuilder, Scene, StandardMaterial, Texture, Vector3} from "@babylonjs/core";
+import {Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, SceneLoader, Vector3} from "@babylonjs/core";
+import { Inspector } from '@babylonjs/inspector';
+
+import meshUrl from "../assets/models/HVGirl.glb";
 
 let engine;
 let canvas;
@@ -7,6 +10,7 @@ window.onload = () => {
     canvas = document.getElementById("renderCanvas");
     engine = new Engine(canvas, true);
     let scene = createScene();
+    Inspector.Show(scene, {});
 
     engine.runRenderLoop(function() {
         scene.render();
@@ -21,8 +25,11 @@ var createScene = function () {
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new Scene(engine);
 
-    var camera = new ArcRotateCamera("camera", 0, 1, 10, Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
+    // This creates and positions a free camera (non-mesh)
+    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
@@ -35,18 +42,14 @@ var createScene = function () {
 
     // Our built-in 'ground' shape.
     var ground = MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
-    const groundMaterial = new StandardMaterial("Ground Material", scene);
-    groundMaterial.diffuseColor = Color3.Red();
-    let groundTexture = new Texture("path/to/checkerboard_basecolor.png", scene);
-    groundMaterial.diffuseTexture = groundTexture; 
-    ground.material = groundMaterial;
 
-    var assetsManager = new AssetsManager(scene);
-    var meshTask = assetsManager.addMeshTask("yeti task", "", "path/to/meshes/", "yeti.babylon");
-    meshTask.onSuccess = function (task) {
-        task.loadedMeshes[0].scaling = new Vector3(0.1, 0.1, 0.1);
-    };
-    assetsManager.load();
+    
+    SceneLoader.ImportMesh("", "", meshUrl, scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        newMeshes[0].name = "girlCharacter";
+        newMeshes[0].scaling = new Vector3(0.1, 0.1, 0.1);
+        camera.target = newMeshes[0];
+    });
 
     return scene;
 };
